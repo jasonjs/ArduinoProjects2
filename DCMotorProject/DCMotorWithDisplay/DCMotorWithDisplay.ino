51253b127b40a5b4468fee4b94a47abba3a97303
jasonjs <jasonjs@yahoo.com>
2015-02-28 15:35:50 -0500

    Startup and slowdown enhancements
    Startup modified to allow for motor startup voltage.  Stopping modified
    to slow down at a more gradual pace.  Hard coded values cleaned up.

--- DCMotorProject/DCMotorWithDisplay/DCMotorWithDisplay.ino
+++ DCMotorProject/DCMotorWithDisplay/DCMotorWithDisplay.ino
@@ -1,14 +1,14 @@
 /*
-  LiquidCrystal Library - Hello World 
- 
+  LiquidCrystal Library - Hello World
+
  Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the 
+ library works with all LCD displays that are compatible with the
  Hitachi HD44780 driver. There are many of them out there, and you
  can usually tell them by the 16-pin interface.
- 
+
  This sketch prints "Hello World!" to the LCD
  and shows the time.
- 
+
  The circuit:
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
@@ -22,7 +22,7 @@
  * 10K resistor:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
- 
+
  Library originally added 18 Apr 2008
  by David A. Mellis
  library modified 5 Jul 2009
@@ -31,9 +31,9 @@
  by Tom Igoe
  modified 22 Nov 2010
  by Tom Igoe
- 
+
  This example code is in the public domain.
- 
+
  http://www.arduino.cc/en/Tutorial/LiquidCrystal
  */
 
@@ -42,13 +42,18 @@
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 4, 6, 5, 3, 2);
-int motorSpeed = 0;
-int maxSpeed = 0;
+int motorSpeed = 0; //current motor speed
+int maxSpeed = 0; //max speed allowed (set at control panel).  This is already adjusted by dividing by 4
 int goButton = 0;
 int stopButton = 0;
 int loopsSinceLastButton = 0;
 int sleepAfterLoops = 30000; //30000 = 5 minutes when each loop is 10ms
 int secondsLeft = 0;
+int delayPerNormalLoop = 8; //Adjust this so that the countdown timer is about 1 second long
+int slackOutSpeedPercentage = 40; //What percent of max speed should the start up hold at to take slack out of train?
+int slackOutSpeedMillis = 1000; //Length of time to allow train to slack out
+int startUpSpeedPercentage = 60; //Just to get the motor turning, hit it with this percentage for the number of milliseconds defined below
+int startUpSpeedMillis = 100; //Length of time to spike voltage to get motor turning
 boolean debugMode = false;
 
 void setup() {
@@ -59,24 +64,24 @@ void setup() {
   pinMode(10, OUTPUT); //stop button light
   pinMode(12, OUTPUT); //go button light
 
-  // set up the LCD's number of columns and rows: 
+  // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.setCursor(0,0);
-  lcd.print("   >> READY <<   ");
+  lcd.setCursor(0, 0);
+  lcd.print(">> Jakes RR v0.3 <<");
 }
 
 void printDebug()
 {
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("G ");
   lcd.print(goButton);
   lcd.print(" S ");
   lcd.print(stopButton);
-  lcd.print(" M ");  
+  lcd.print(" M ");
   lcd.print(maxSpeed);
   lcd.print("       ");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("DEBUG MODE      ");
 }
 
@@ -86,7 +91,7 @@ void printSpeed()
   speedPercentage = speedPercentage * 100;
   float maxSpeedPercentage = (float)maxSpeed / (float)256;
   maxSpeedPercentage = maxSpeedPercentage * 100;
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("S ");
   lcd.print((int)speedPercentage);
   lcd.print("% M ");
@@ -103,37 +108,37 @@ void loop() {
   goButton = analogRead(4);
   stopButton = analogRead(3);
   secondsLeft = (sleepAfterLoops - loopsSinceLastButton) / 100;
-  if(debugMode)
+  if (debugMode)
   {
     printDebug();
-    delay(10);
+    delay(delayPerNormalLoop);
   }
   else
   {
-    delay(8);
-    if(loopsSinceLastButton >= sleepAfterLoops)
+    delay(delayPerNormalLoop);
+    if (loopsSinceLastButton >= sleepAfterLoops)
     {
       //if it's been too long since a button was pressed, press the stop button
-      stopButton = 513;      
+      stopButton = 513;
     }
 
-    if(goButton > 512)
+    if (goButton > 512)
     {
-      digitalWrite(10,LOW);
-      digitalWrite(12,HIGH);
+      digitalWrite(10, LOW);
+      digitalWrite(12, HIGH);
       runForward();
       loopsSinceLastButton = 0;
     }
-    else if(stopButton > 512)
+    else if (stopButton > 512)
     {
-      digitalWrite(12,LOW);
-      digitalWrite(10,HIGH);
-      stopMotor(); 
+      digitalWrite(12, LOW);
+      digitalWrite(10, HIGH);
+      stopMotor();
       loopsSinceLastButton = 0;
     }
     printSpeed();
   }
-  if(motorSpeed > 0)
+  if (motorSpeed > 0)
   {
     loopsSinceLastButton++;
   }
@@ -142,7 +147,7 @@ void loop() {
 void printString(String s, int line)
 {
   lcd.setCursor(0, line);
-  if(s.length() > 16)
+  if (s.length() > 16)
   {
     //do nothing, the string is too long to add spaces to
   }
@@ -150,7 +155,7 @@ void printString(String s, int line)
   {
     int spacesToAdd = 16 - s.length();
     String spaces = String(' ');
-    for(int i = 0; i < spacesToAdd; i++)
+    for (int i = 0; i < spacesToAdd; i++)
     {
       spaces = String(spaces + ' ');
     }
@@ -159,22 +164,50 @@ void printString(String s, int line)
   }
 }
 
+void setEngineSpeed(int toSpeed)
+{
+  analogWrite(9, motorSpeed);
+  printSpeed();
+}
+
 void accelerateTo(int toSpeed)
 {
-  for( ; motorSpeed < toSpeed && motorSpeed < maxSpeed; motorSpeed++)
+    int slackout = 0;
+  
+  if(motorSpeed != maxSpeed) //engine is already underway.  go button pressed but already going
   {
-    analogWrite(9,motorSpeed);
+    float slackOutSpeed = (float)maxSpeed * ((float)slackOutSpeedPercentage / 100);
+    float startSpikeSpeed = (float)maxSpeed * ((float)startUpSpeedPercentage / 100);
+    motorSpeed = (int)startSpikeSpeed;
+    setEngineSpeed(motorSpeed);
+    delay(startUpSpeedMillis);
+    motorSpeed = (int)slackOutSpeed;
+  }
+  for ( ; motorSpeed < toSpeed && motorSpeed < maxSpeed; motorSpeed++)
+  {
+    setEngineSpeed(motorSpeed);
+    if (slackout == 0)
+    {
+      delay(slackOutSpeedMillis);
+      slackout = 1;
+    }
     delay(10);
-    printSpeed();
   }
 }
 
 void decelerateTo(int toSpeed)
 {
-  for( ; motorSpeed > toSpeed; motorSpeed--)
+  for ( ; motorSpeed > toSpeed; motorSpeed--)
   {
-    analogWrite(9,motorSpeed);
-    delay(10);
+    setEngineSpeed(motorSpeed);
+    if (motorSpeed < 40) //slow the rate of deceleration
+    {
+      delay(60);
+    }
+    else
+    {
+      delay(20);
+    }
     printSpeed();
   }
 }
@@ -182,27 +215,27 @@ void decelerateTo(int toSpeed)
 
 void runForward()
 {
-  printString("GO FORWARD!",0);
-  digitalWrite(11,LOW); /*MOROTSHIELD_IN2 = 11 */
-  digitalWrite(8,HIGH); /*MOROTSHIELD_IN1 = 8 */
+  printString("GO FORWARD!", 0);
+  digitalWrite(11, LOW); /*MOROTSHIELD_IN2 = 11 */
+  digitalWrite(8, HIGH); /*MOROTSHIELD_IN1 = 8 */
   accelerateTo(256);
-  printString("CHOO CHOO!",0);
+  printString("CHOO CHOO!", 0);
 }
 
 void runBackward()
 {
-  digitalWrite(8,LOW); /*MOROTSHIELD_IN1 = 8 */
-  digitalWrite(11,HIGH); /*MOROTSHIELD_IN1 = 8 */
+  digitalWrite(8, LOW); /*MOROTSHIELD_IN1 = 8 */
+  digitalWrite(11, HIGH); /*MOROTSHIELD_IN1 = 8 */
   accelerateTo(256);
 }
 
 void stopMotor()
 {
-  printString("STOP!",0);
+  printString("STOP!", 0);
   decelerateTo(0);
-  digitalWrite(8,LOW); /*MOROTSHIELD_IN1 = 8 */
-  digitalWrite(11,LOW); /*MOROTSHIELD_IN1 = 8 */
-  printString("STOPPED",0);
+  digitalWrite(8, LOW); /*MOROTSHIELD_IN1 = 8 */
+  digitalWrite(11, LOW); /*MOROTSHIELD_IN1 = 8 */
+  printString("STOPPED", 0);
 }
 
 
